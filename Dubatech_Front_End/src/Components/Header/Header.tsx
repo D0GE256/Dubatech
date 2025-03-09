fd39d94df81a93ae02bf86ecfc1841c762c30e4c
Alejandro <jagsalejandrog2@gmail.com>
2025-03-09 11:10:18 +0100

    .

--- Dubatech_Front_End/src/Components/Header/Header.tsx
+++ Dubatech_Front_End/src/Components/Header/Header.tsx
@@ -1,13 +1,20 @@
-import Dubatech from "../../assets/DubatechBg.png"
-import "./Header.css"
-const Header = () => {
+    import Dubatech from "../../assets/DubatechBg.png";
+    import "./Header.css";
+    const Header = () => {
     return (
-        <div className="header">
+        <div id="header">
+        <div className="logoDiv">
             <img className="logo" src={Dubatech} alt="Dubatech logo" />
             <strong className="logoNombre">Dubatech</strong>
-            <a href=""></a>
         </div>
-    )
-}
+        <div className="strongs">
+            <strong className="Inicio">Inicio</strong>
+            <strong className="Servicios">Servicios</strong>
+            <strong className="Nosotros">Nosotros</strong>
+            <strong className="Contacto">Contacto</strong>
+        </div>
+        </div>
+    );
+    };
 
-export default Header
+    export default Header;
