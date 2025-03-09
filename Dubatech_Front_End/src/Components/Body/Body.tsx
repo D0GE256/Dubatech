0e3129d16d0ef937a6da4ca693e348a787eac0a5
Alejandro <jagsalejandrog2@gmail.com>
2025-03-09 15:27:22 +0100

    .

---
+++ Dubatech_Front_End/src/Components/Body/Body.tsx
@@ -0,0 +1,23 @@
+import "./Body.css";
+import Servers from "../../assets/servers.webp";
+
+const Body = () => {
+  return (
+    <div className="row">
+      <div className="text">
+        <h1 className="t1 margin textRight">Propuesta de valor</h1>
+        <p className="p1 margin textRight">
+          En Dubatech, ofrecemos productos y soluciones tecnológicas innovadoras
+          para empresas y profesionales que buscan optimizar sus procesos,
+          mejorar su presencia digital y potenciar su crecimiento. Nos dirigimos
+          a empresas, emprendedores y profesionales independientes que buscan
+          tecnología de vanguardia, consultoría especializada y servicios
+          digitales adaptados a sus necesidades.
+        </p>
+      </div>
+      <img className="img" src={Servers} alt="Servers" />
+    </div>
+  );
+};
+
+export default Body;
--- Dubatech_Front_End/src/Components/Header/Header.tsx
+++ Dubatech_Front_End/src/Components/Header/Header.tsx
@@ -1,20 +1,21 @@
-    import Dubatech from "../../assets/DubatechBg.png";
-    import "./Header.css";
-    const Header = () => {
-    return (
-        <div id="header">
-        <div className="logoDiv">
-            <img className="logo" src={Dubatech} alt="Dubatech logo" />
-            <strong className="logoNombre">Dubatech</strong>
-        </div>
-        <div className="strongs">
-            <strong className="Inicio">Inicio</strong>
-            <strong className="Servicios">Servicios</strong>
-            <strong className="Nosotros">Nosotros</strong>
-            <strong className="Contacto">Contacto</strong>
-        </div>
-        </div>
-    );
-    };
+import Dubatech from "../../assets/DubatechBg.png";
+import "./Header.css";
 
-    export default Header;
+const Header = () => {
+  return (
+    <div id="header">
+      <div className="logoDiv">
+        <img className="logo" src={Dubatech} alt="Dubatech logo" />
+        <strong className="logoNombre">Dubatech</strong>
+      </div>
+      <div className="strongs">
+        <strong className="Inicio">Inicio</strong>
+        <strong className="Servicios">Servicios</strong>
+        <strong className="Nosotros">Nosotros</strong>
+        <strong className="Contacto">Contacto</strong>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
